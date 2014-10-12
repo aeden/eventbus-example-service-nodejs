c04dca45c2adadc13d0e4df3b346a6a5ade68646
Anthony Eden <anthonyeden@gmail.com>
2014-10-12 23:10:58 +0200

    Implement retry logic.

--- eventhandler.js
+++ eventhandler.js
@@ -35,45 +35,69 @@ deliver = function(message) {
   req.end();
 };
 
-ws.on('open', function() {
-  console.log("Connected");
-  retry_count = 0;
-  ws.send(JSON.stringify({action: 'authenticate', credentials: process.env.AUTHORIZATION_TOKEN}));
-});
-
-ws.on('message', function(msg, flags) {
-  console.log('Received message: %s', msg);
-  var evt = JSON.parse(msg);
-
-  switch (evt['name']) {
-    case 'check-domain':
-      sleep.sleep(1);
-
-      var results = [];
-      for (var i = 0, len = evt['data'].length; i < len; i++) {
-        results.push({name: evt['data'][i], availability: 'available'});
-      }
-
-      message = {name: 'check-domain-completed', data: results, context: evt['context']};
-      deliver(JSON.stringify(message));
-      break;
-    case 'register-domain':
-      received_data = evt['data'];
-
-      sleep.sleep(2);
-
-      results = {
-        name: received_data['name'],
-        registered: true,
-        expiration: ''
-      };
-
-      message = JSON.stringify({name: 'register-domain-completed', data: results, context: evt['context']});
-      deliver(message);
-      break;
+connect = function(retryCount) {
+  retryCount = (typeof retryCount === "undefined") ? 0 : retryCount;
+  if (retryCount > 20) {
+    throw new Error("Max connection attempts reached");
   }
-});
 
-ws.on('close', function() {
-  console.log("Disconnected");
-});
+  if (retryCount > 0) {
+    console.log("Connecting in %s seconds", retryCount);
+    sleep.sleep(retryCount);
+  }
+
+  console.log("Opening websocket connection");
+  var ws = new WebSocket(WEBSOCKET_URL, {origin: 'http://localhost:3000'});
+
+  ws.on('open', function() {
+    console.log("Connected");
+    retryCount = 0;
+    ws.send(JSON.stringify({action: 'authenticate', credentials: process.env.AUTHORIZATION_TOKEN}));
+  });
+
+  ws.on('message', function(msg, flags) {
+    console.log('Received message: %s', msg);
+    var evt = JSON.parse(msg);
+
+    switch (evt['name']) {
+      case 'check-domain':
+        sleep.sleep(1);
+
+        var results = [];
+        for (var i = 0, len = evt['data'].length; i < len; i++) {
+          results.push({name: evt['data'][i], availability: 'available'});
+        }
+
+        message = {name: 'check-domain-completed', data: results, context: evt['context']};
+        deliver(JSON.stringify(message));
+        break;
+      case 'register-domain':
+        received_data = evt['data'];
+
+        sleep.sleep(2);
+
+        results = {
+          name: received_data['name'],
+          registered: true,
+          expiration: ''
+        };
+
+        message = JSON.stringify({name: 'register-domain-completed', data: results, context: evt['context']});
+        deliver(message);
+        break;
+    }
+  });
+
+  ws.on('close', function() {
+    console.log("Disconnected");
+    connect(retryCount + 1);
+  });
+
+  ws.on('error', function(err) {
+    console.log("Error: %s", err);
+    connect(retryCount + 1);
+  });
+
+};
+
+connect();
